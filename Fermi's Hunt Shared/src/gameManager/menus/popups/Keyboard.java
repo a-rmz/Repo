848e181448e1532ae6cd5704fae466803acbedba
a-rmz <armzprz@gmail.com>
2015-12-05 16:37:30 -0600

    Keyboard comentado

--- Fermi's Hunt Shared/src/gameManager/menus/popups/Keyboard.java
+++ Fermi's Hunt Shared/src/gameManager/menus/popups/Keyboard.java
@@ -9,39 +9,80 @@ import java.awt.Rectangle;
 
 public class Keyboard {
 	
+	/**
+	 * Private inner class for Keyboard.
+	 * @author alex
+	 *
+	 */
 	private class Key {
 		public char keyVal;
 		public boolean use;
 		protected Rectangle keyForm;
 		
+		/**
+		 * Adds a key by its char value.
+		 * @param keyVal
+		 */
 		public Key(char keyVal) {
 			this.keyVal = keyVal;
 			this.use = false;
 		}
+		/**
+		 * Adds a key by the int representation of its char value.
+		 * @param i
+		 */
 		public Key(int i) {
 			keyVal = (char) i;
 			this.use = false;
 		}
 
+		/**
+		 * Returns the char value of the key.
+		 * @return value of the key
+		 */
 		public char getKeyVal() {
 			return keyVal;
 		}
+		/**
+		 * Returns if the key is being used.
+		 * @return
+		 */
 		public boolean used() {
 			return use;
 		}
+		/**
+		 * Sets the key to using.
+		 */
 		public void using() {
 			use = true;
 		}
+		/**
+		 * Sets the key to not using.
+		 */
 		public void unused() {
 			use = false;
 		}
+		/**
+		 * Sets the rectangle of the key with the given one.
+		 * @param keyForm
+		 */
 		public void setForm(Rectangle keyForm) {
 			this.keyForm = keyForm;
 		}
+		/**
+		 * Returns the form of the rectangle.
+		 * @return rectangle with the key shape
+		 */
 		public Rectangle getForm() {
 			return keyForm;
 		}
 		
+		/**
+		 * Draws the key on the given coordinates.
+		 * @param g
+		 * @param x
+		 * @param y
+		 */
 		public void drawKey(Graphics2D g, int x, int y) {
 			String kVal = "" + keyVal;
 			Font ft = new Font("8-Bit Madness", Font.PLAIN, 80);
@@ -60,19 +101,37 @@ public class Keyboard {
 							);
 		}
 	}
-	
+
+	/**
+	 * Private inner class for Keyboard. For keys with non-character direct representation: ENTER, SPACE, BACKSPACE.
+	 * @author alex
+	 *
+	 */
 	private class SpecialKey extends Key {
 		private String kName;
 		
+		/**
+		 * Calls the Super constructor with the char value of the key.
+		 * @param keyVal
+		 * @param name
+		 */
 		public SpecialKey(char keyVal, String name) {
 			super(keyVal);
 			this.kName = name;
 		}
+		/**
+		 * Calls the Super constructor with the int representation of the char value of the key.
+		 * @param keyVal
+		 * @param name
+		 */
 		public SpecialKey(int keyVal, String name) {
 			super(keyVal);
 			this.kName = name;
 		}
 		
+		/**
+		 * Draws the special key.
+		 */
 		@Override
 		public void drawKey(Graphics2D g, int x, int y) {
 			Font ft = new Font("8-Bit Madness", Font.PLAIN, 60);
@@ -97,15 +156,16 @@ public class Keyboard {
 	private SpecialKey[] specialKeys;
 	private Key previousUsed;
 	
+	/**
+	 * Initializes the keyboard.
+	 */
 	public Keyboard() {
-		init();
-	}
-	
-	private void init() {
 		initKeyboard();
-		
 	}
 	
+	/**
+	 * Sets the keys, and their shape rectangles.
+	 */
 	private void initKeyboard() {
 		keyboard = new Key[4][10];
 		specialKeys = new SpecialKey[3];
@@ -144,18 +204,45 @@ public class Keyboard {
 		}
 	}
 
+	/**
+	 * Returns the shape Rectangle of the given key.
+	 * @param i
+	 * @param j
+	 * @return
+	 */
 	public Rectangle getKeyShape(int i, int j) {
 		return keyboard[i][j].getForm();
 	}
+	/**
+	 * Returns the char value of the given key.
+	 * @param i
+	 * @param j
+	 * @return
+	 */
 	public char getKeyValue(int i, int j) {
 		return keyboard[i][j].getKeyVal();
 	}
+	/**
+	 * Returns the shape Rectangle of the given special key.
+	 * @param i
+	 * @return
+	 */
 	public Rectangle getSpecialKeyShape(int i) {
 		return specialKeys[i].getForm();
 	}
+	/**
+	 * Returns the char value of the given special key.
+	 * @param i
+	 * @return
+	 */
 	public char getSpecialKeyValue(int i) {
 		return specialKeys[i].getKeyVal();
 	}
+	/**
+	 * Changes the used key to the new one.
+	 * @param i
+	 * @param j
+	 */
 	public void usedKey(int i, int j) {
 		previousUsed.unused();
 		if(j == -1) {
@@ -167,6 +254,12 @@ public class Keyboard {
 		}
 	}
 	
+	/**
+	 * Draws the keys on the given coordinates.
+	 * @param g
+	 * @param x
+	 * @param y
+	 */
 	public void drawKeyboard(Graphics2D g, int x, int y) {
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 10; j++) {
@@ -179,6 +272,10 @@ public class Keyboard {
 		
 	}
 	
+	/**
+	 * Draws the whole keyboard, including the keys.
+	 * @param g
+	 */
 	public void draw(Graphics2D g){
 		int x = 200, y = 470;
 		g.setStroke(new BasicStroke(6));
