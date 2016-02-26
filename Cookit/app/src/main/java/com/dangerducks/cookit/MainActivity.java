eaabffdab15ac36dfa78b8fa5c326375db3ff4d6
a-rmz <armzprz@gmail.com>
2016-02-26 07:53:54 -0600

    Added drawer menu

--- Cookit/app/src/main/java/com/dangerducks/cookit/MainActivity.java
+++ Cookit/app/src/main/java/com/dangerducks/cookit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dangerducks.cookit;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
@@ -9,10 +12,44 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] planets;
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        // Home menu for drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setUpDrawer();
+    }
+
+    private void setUpDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer) {
+            public void onDrawerOpened(View view) {
+                super.onDrawerOpened(view);
+                getSupportActionBar().setTitle(R.string.open_drawer);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -20,10 +57,38 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
 
+        planets = getResources().getStringArray(R.array.planets);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView = (ListView) findViewById(R.id.left_drawer);
+
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.list_item, planets));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
     public boolean onOptionsItemSelected(MenuItem menuItem) {
+
+        if (mDrawerToggle.onOptionsItemSelected(menuItem)) {
+            return true;
+        }
         switch (menuItem.getItemId()) {
             case R.id.action_search:
                 nothingToDoHere();
